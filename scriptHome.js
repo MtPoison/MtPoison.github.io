e7b802d3ec5165afd1623cea3ca58f3438720545
MtPoison <qleforestier@gaming.tech>
2024-10-08 18:06:45 +0200

    ajout de nouvelle card

--- scriptHome.js
+++ scriptHome.js
@@ -1,26 +1,57 @@
-let slideIndex = 0;
- showSlides();
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000);
+document.addEventListener('DOMContentLoaded', function() {
+  const categoryFilter = document.getElementById('category-filter');
+  const categories = document.querySelectorAll('.category');
+
+  categoryFilter.addEventListener('change', function() {
+    const selectedCategory = categoryFilter.value;
+
+    categories.forEach(category => {
+      if (selectedCategory === 'all') {
+        category.style.display = 'block';
+      } else {
+        if (category.getAttribute('data-category') === selectedCategory) {
+          category.style.display = 'block';
+        } else {
+          category.style.display = 'none';
+        }
+      }
+    });
+  });
+
+  const projectCards = document.querySelectorAll('.project-card');
+  projectCards.forEach(card => {
+    card.addEventListener('click', function() {
+      const url = card.getAttribute('data-url');
+      window.open(url, '_blank');
+    });
+  });
+});
+
+fetch('projet.json')
+    .then(response => response.json())
+    .then(data => {
+        const projectCards = document.querySelectorAll('.project-card');
+        
+        projectCards.forEach(card => {
+            card.addEventListener('click', () => {
+                const projectKey = card.getAttribute('data-url');
+                const projectData = data.cpp.find(project => project.title.toLowerCase().includes(projectKey));
+
+                if (projectData) {
+                    displayProjectDetails(projectData);
+                }
+            });
+        });
+    })
+    .catch(error => console.error('Erreur lors du chargement du JSON:', error));
+
+// Fonction pour afficher les détails du projet
+function displayProjectDetails(project) {
+    const projectDetails = document.getElementById('project-details');
+    projectDetails.innerHTML = `
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <a href="${project.link}" target="_blank">Voir le projet</a>
+    `;
 }
 
-let slideIndex2 = 0;
-showSlides2();
-function showSlides2() {
-  let slides2 = document.getElementsByClassName("slide2");
-  for (let i = 0; i < slides2.length; i++) {
-    slides2[i].style.display = "none";
-  }
-  slideIndex2++;
-  if (slideIndex2 > slides2.length) {slideIndex2 = 1}
-  slides2[slideIndex2-1].style.display = "block";
-  setTimeout(showSlides2, 5000);
-}
-  
